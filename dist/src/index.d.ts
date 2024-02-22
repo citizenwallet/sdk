9a1e3ea49dea70393df6bc68d1c652ddc5db68ea
kev-techi <kevin@techi.be>
2024-02-22 12:13:56 +0100

    delete dist

--- dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ERC20IOU, useERC20IOU } from "./state/erc20IOU";
-export { ERC20IOUContract } from "./contracts/ERC20IOU";
-export { getLocalHash, getSignedHash } from "./contracts/ERC20IOU/utils";
-//# sourceMappingURL=index.d.ts.map
