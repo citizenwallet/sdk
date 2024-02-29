28de33c271649533126ffbb038da10392e1b481e
kev-techi <kevin@techi.be>
2024-02-29 12:20:33 +0100

    add base faucet factory

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,5 +3,5 @@ export const CONFIG_BASE_URL = "https://config.internal.citizenwallet.xyz";
 export const FaucetFactories: { [key: string]: string } = {
   "42220": "0xAE76B1C6818c1DD81E20ccefD3e72B773068ABc9",
   "137": "0xcF51C007E829b0080f5D3bC397C36993dC210856",
-  "8453": "0xAE76B1C6818c1DD81E20ccefD3e72B773068ABc9",
+  "8453": "0x5cC010B9e02Ff807F78b8A18C8aC02019b6F90EB",
 };
