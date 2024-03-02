4021d354047cd42a44e7fb2c435110183e1a49eb
kev-techi <kevin@techi.be>
2024-03-02 02:45:32 +0100

    add voucher factory

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,3 +5,9 @@ export const FaucetFactories: { [key: string]: string } = {
   "137": "0xcF51C007E829b0080f5D3bC397C36993dC210856",
   "8453": "0x5cC010B9e02Ff807F78b8A18C8aC02019b6F90EB",
 };
+
+export const VoucherFactories: { [key: string]: string } = {
+  "42220": "0x38819D2D05C6cAaB38B1C16F944acC9E023E010f",
+  "137": "0x111010B9e02Ff807F78b8A18C8aC02019b6F9111",
+  "8453": "0x111010B9e02Ff807F78b8A18C8aC02019b6F9111",
+};
