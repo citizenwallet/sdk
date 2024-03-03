3c4ce686f6d970eca8c41890d16a47fd071c4be4
kev-techi <kevin@techi.be>
2024-03-03 16:42:56 +0100

    added some comments

--- src/state/erc20/index.ts
+++ src/state/erc20/index.ts
@@ -13,6 +13,9 @@ import { delay } from "../../utils/delay";
 
 type erc20StoreSelector<T> = (state: ERC20Store) => T;
 
+/**
+ * Represents a class that provides actions related to ERC20 tokens.
+ */
 export class ERC20Actions {
   store: StoreApi<ERC20Store>;
 
@@ -33,6 +36,10 @@ export class ERC20Actions {
     this.indexerService = new IndexerService(configIndexer);
   }
 
+  /**
+   * Retrieves the balance of the specified address.
+   * @param address The address for which to retrieve the balance.
+   */
   async getBalance(address: string) {
     try {
       this.store.getState().balanceRequest();
@@ -49,6 +56,14 @@ export class ERC20Actions {
   private fetchLimit = 10;
   private transfersPagination?: IndexerResponsePaginationMetadata;
   private previousFetchLength = 0;
+
+  /**
+   * Retrieves transfers for a given address.
+   *
+   * @param address - The address for which to retrieve transfers.
+   * @param reset - Indicates whether to reset the state before fetching transfers.
+   * @returns A promise that resolves to a boolean indicating whether the transfers were successfully retrieved.
+   */
   async getTransfers(address: string, reset = false): Promise<boolean> {
     try {
       if (reset) {
@@ -98,11 +113,24 @@ export class ERC20Actions {
     return false;
   }
 
+  /**
+   * Fetches transfers for a given address automatically based on scrolling behavior in a given div.
+   * This allows you to easily implement infinite scrolling for transfers.
+   * The function will automatically stop fetching when there are no more transfers to fetch.
+   *
+   * If the div isn't scrollable yet, it will keep trying to fetch until it is.
+   *
+   * @param address - The address for which transfers are to be fetched.
+   * @param scrollableRef - A reference to the scrollable element.
+   * @param cleanupFunctionRef - A mutable reference to the cleanup function.
+   * @param refetchDelay - The delay (in milliseconds) between subsequent fetches. Default is 500ms.
+   */
   async getTransfersForScrollable(
     address: string,
     scrollableRef: React.RefObject<HTMLDivElement>,
+    cleanupFunctionRef: React.MutableRefObject<() => void>,
     refetchDelay = 500
-  ): Promise<() => void> {
+  ) {
     try {
       const isScrollable = isRefScrollable(scrollableRef);
 
@@ -110,12 +138,20 @@ export class ERC20Actions {
         // try and fetch until we have enough to scroll, stop if we have no more to fetch
         const hasMore = await this.getTransfers(address);
         if (!hasMore) {
-          return () => {};
+          cleanupFunctionRef.current = () => {};
+          return;
         }
 
         await delay(refetchDelay);
 
-        return this.getTransfersForScrollable(address, scrollableRef);
+        this.getTransfersForScrollable(
+          address,
+          scrollableRef,
+          cleanupFunctionRef,
+          refetchDelay
+        );
+
+        return;
       }
 
       const listener = () => {
@@ -132,15 +168,25 @@ export class ERC20Actions {
 
       scrollableRef.current?.addEventListener("scroll", listener);
 
-      return () => {
+      cleanupFunctionRef.current = () => {
         // remove listener
         scrollableRef.current?.removeEventListener("scroll", listener);
       };
+      return;
     } catch (error) {}
-    return () => {};
+    cleanupFunctionRef.current = () => {};
+    return;
   }
 }
 
+/**
+ * Custom hook for interacting with ERC20 tokens.
+ * This provides state management functionality where you can call actions that trigger state changes.
+ * You can listen to state changes on your UI components and update them accordingly.
+ *
+ * @param config - The configuration object.
+ * @returns An array containing a selector function and ERC20 actions.
+ */
 export const useERC20 = (
   config: Config
 ): [<T>(selector: erc20StoreSelector<T>) => T, ERC20Actions] => {
@@ -158,6 +204,11 @@ export const useERC20 = (
     [address, wsUrl, rpcUrl, config.indexer]
   );
 
+  /**
+   * Custom hook for accessing the ERC20 store.
+   * @param selector - The selector function.
+   * @returns The selected value from the ERC20 store.
+   */
   const useBoundStore = <T>(selector: erc20StoreSelector<T>) =>
     useStore(configActions.store, selector);
 
