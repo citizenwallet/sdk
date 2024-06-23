dab97e8897ba961d4d66f2e4d24db4f8ef5f6f4b
kev-techi <kevin@techi.be>
2024-06-23 18:14:39 +0200

    add voucher parsing

---
+++ src/services/vouchers/index.ts
@@ -0,0 +1,46 @@
+import { BaseWallet, SigningKey } from "ethers";
+import { decompress } from "../../utils/deeplink";
+
+export interface Voucher {
+  alias: string;
+  creator: string;
+  account: string;
+  name: string;
+}
+
+export const parseVoucher = (data: string) => {
+  const url = new URL(data.replace("#/", ""));
+
+  const voucherKey = url.searchParams.get("voucher");
+  if (!voucherKey) {
+    throw new Error("Invalid voucher");
+  }
+
+  const decodedKey = decompress(voucherKey);
+
+  const signingKey = new SigningKey(
+    decodedKey.replace("00", "").replace("v2-", "0x")
+  );
+  const signer = new BaseWallet(signingKey);
+
+  const voucherParams = url.searchParams.get("params");
+  if (!voucherParams) {
+    throw new Error("Invalid voucher");
+  }
+
+  const decodedParams = decompress(voucherParams);
+
+  const voucherSearchParams = new URLSearchParams(decodedParams);
+
+  const voucher: Voucher = {
+    alias: voucherSearchParams.get("alias") || "",
+    creator: voucherSearchParams.get("creator") || "",
+    account: voucherSearchParams.get("account") || "",
+    name: voucherSearchParams.get("name") || "Voucher",
+  };
+  if (!voucher.alias || !voucher.creator || !voucher.account) {
+    throw new Error("Invalid voucher");
+  }
+
+  return { voucher, signer };
+};
