aa8b7a0d17cb554f2d2de6c2c6a22577d7d2d661
kev-techi <kevin@techi.be>
2024-09-20 11:23:04 +0200

    add voucher creation function

--- src/services/vouchers/index.ts
+++ src/services/vouchers/index.ts
@@ -1,5 +1,7 @@
 import { Wallet, SigningKey } from "ethers";
-import { decompress } from "../../utils/deeplink";
+import { compress, decompress } from "../../utils/deeplink";
+import { ConfigService } from "../config";
+import { AccountFactoryService } from "../contracts/AccountFactory";
 
 export interface Voucher {
   alias: string;
@@ -8,6 +10,22 @@ export interface Voucher {
   name: string;
 }
 
+/**
+ * Parses a voucher from a given data string (typically a URL).
+ *
+ * @param data - A string containing the voucher data, usually in URL format.
+ * @returns An object containing the parsed voucher information and a signer.
+ * @throws Error if the voucher data is invalid or incomplete.
+ *
+ * @remarks
+ * This function performs the following steps:
+ * 1. Parses the input string as a URL.
+ * 2. Extracts and decodes the voucher key (signer's private key).
+ * 3. Creates a signer from the decoded key.
+ * 4. Extracts and decodes the voucher parameters.
+ * 5. Constructs a Voucher object from the decoded parameters.
+ * 6. Validates the voucher data for completeness.
+ */
 export const parseVoucher = (data: string) => {
   const url = new URL(data.replace("#/", ""));
 
@@ -43,4 +61,55 @@ export const parseVoucher = (data: string) => {
   }
 
   return { voucher, signer };
+};
+
+/**
+ * Creates a voucher for a given community.
+ *
+ * @param communityAlias - The alias of the community for which the voucher is created.
+ * @param voucherName - The name of the voucher.
+ * @param voucherCreator - The account address of creator. Since the user redeeming the voucher will only see a transaction from the voucher to them, this allows to display the original creator.
+ * @param voucherSigner - A newly generated wallet that will be used to sign transactions for this voucher.
+ * @returns An object containing the voucher link and the voucher account address.
+ *
+ * @remarks
+ * - A new voucherSigner (wallet) should be generated for each voucher to ensure security.
+ * - This function creates a voucher but does not fund it. Tokens need to be sent to the
+ *   voucher account address for it to be considered "unredeemed". Without funds, the voucher
+ *   will be considered "redeemed" or empty.
+ * - The voucherSigner's private key is included in the voucher link, so it's crucial to generate
+ *   a new key for each voucher to prevent reuse and potential security issues.
+ */
+export const createVoucher = async (
+  communityAlias: string,
+  voucherName: string,
+  voucherCreator: string,
+  voucherSigner: Wallet
+) => {
+  const configService = new ConfigService();
+  const config = await configService.getBySlug(communityAlias);
+
+  const accountFactory = new AccountFactoryService(
+    config.erc4337.account_factory_address,
+    voucherSigner
+  );
+  const voucherAccountAddress = await accountFactory.getAddress();
+
+  const voucherParams = new URLSearchParams();
+  voucherParams.set("alias", communityAlias);
+  voucherParams.set("creator", voucherCreator);
+  voucherParams.set("account", voucherAccountAddress);
+  voucherParams.set("name", voucherName);
+
+  const voucherString = compress(voucherParams.toString());
+
+  const voucherKey = `v2-${voucherSigner.privateKey
+    .replace("00", "") // there are sometimes 00s in the private key
+    .replace("0x", "")}`;
+
+  const encodedVoucherKey = compress(voucherKey);
+
+  const voucherLink = `${process.env.DEEPLINK_URL}/#/?voucher=${encodedVoucherKey}&params=${voucherString}`;
+
+  return { voucherLink, voucherAccountAddress };
 };
